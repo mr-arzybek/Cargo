8b218cc65aac87020f803b1dbb9313cb2dcad7d8
Arzybek Abdyrakhmanov <abdyrakhmanov.arzybek@gmail.com>
2023-10-12 21:42:10 +0600

    add

--- cargo_app/admin.py
+++ cargo_app/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import TrackCode, Status
 
-admin.site.register(TrackCode)
-admin.site.register(Status)
+
+class TrackCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'status')
+    list_filter = ('status',)
+    search_fields = ('code',)
+    list_per_page = 20
+
+
+admin.site.register(TrackCode, TrackCodeAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name_status',)
+    search_fields = ('name_status',)
+    list_per_page = 20
+
+
+admin.site.register(Status, StatusAdmin)
