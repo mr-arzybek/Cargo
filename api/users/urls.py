f55fd69ef4cc7e2614db7ab941b25c55790aa41a
mirlanchik1337 <sadambekovmirlan9@gmail.com>
2023-10-22 00:51:32 +0600

    asd

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from api.users import views
 from api.users.views import PasswordResetRequestAPIView, PasswordResetCodeAPIView, PasswordResetNewPasswordAPIView, \
@@ -10,4 +11,8 @@ urlpatterns = [
     path("reset-password-code/", PasswordResetCodeAPIView.as_view(), name="write code"),
     path("reset-new-password/<str:code>/", PasswordResetNewPasswordAPIView.as_view(), name="write new password"),
     path('login/', LoginView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
