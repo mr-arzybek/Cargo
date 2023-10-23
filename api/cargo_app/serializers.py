c1fdf64ad83b2c815042f608a4b571e8ef88c4eb
mirlanchik1337 <sadambekovmirlan9@gmail.com>
2023-10-23 16:55:41 +0600

    asd

--- api/cargo_app/serializers.py
+++ api/cargo_app/serializers.py
@@ -6,7 +6,7 @@ from api.cargo_app.models import Status, TrackCode
 class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
-        exclude = 'id'.split()
+        fields = '__all__'
 
 
 class TrackCodeSerializer(serializers.ModelSerializer):
