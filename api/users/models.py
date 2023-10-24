15076ce6265fcc4a0e2ce60042ab365fb2b7b51e
mirlanchik1337 <sadambekovmirlan9@gmail.com>
2023-10-24 17:26:29 +0600

    awde

--- api/users/models.py
+++ api/users/models.py
@@ -7,20 +7,23 @@ from django.db import models
 class UserCustomManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, password):
-        user = self.model(
-        email = self.normalize_email(email),
-                password = password,)
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        self.create_user(email,password )
-        self.user.is_staff()
-        self.user.is_superuser = True
-        self.user.save()
-        return self.user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
         return self.create_user(email, password, **extra_fields)
 
